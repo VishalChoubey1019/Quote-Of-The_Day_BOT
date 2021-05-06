188f5ae6721f4e7ea376b2e8cde795a443332124
Vishal Choubey <74164476+VishalChoubey1019@users.noreply.github.com>
2021-05-06 22:30:39 +0530

    Update quote-bot__code.py

--- quote-bot__code.py
+++ quote-bot__code.py
@@ -8,7 +8,6 @@ import time
 
 client = commands.Bot(command_prefix = 'q_')
 
-# Function to find quote # 
 def find_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
@@ -16,32 +15,45 @@ def find_quote():
   return (quote)
 
 
+@tasks.loop(seconds=43200)   #43200 = 12*60*60 secs = 12 hours, you can change it to any amount of time
+async def play_quotes(ctx):
+  name = ' '
+  quote_with_name    = find_quote()
+
+  quote_without_name = quote_with_name.split('-')[0]
+  b = 0
+
+  for i in range(0,len(quote_with_name)):  # This thing is only for adding quotes(" ") in the part without the name of the writer/speaker.
+                                           # But this feature fails when sometimes the quote has a ' - ' before writer/speaker's name.
+    if quote_with_name[i] == '-':
+      b = 1                       
+    if b==1:
+      name = name + quote_with_name[i]     
+
+  await ctx.send(f'**"{quote_without_name}" {name}**') 
+
+@client.command()                          # You can check the ping here.
+async def ping(ctx):
+  await ctx.send(f'Ping : {round(client.latency * 1000)} ms')  # ms is milisecond. 
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Invalid Command Used. Type q_help to know the commands')
+                       
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Give proper values to the command an argument is missing')
+
 @client.event 
 async def on_ready():
-  print('Hey m turned on :p')    #this is printed on my terminal
+  print('Hey m turned on :p')   # This is printed on the website i host it on.
 
 
 @client.command()
 async def inspire(ctx):
-  while True:
-    
-    name = ' '
-    quote_with_name    = find_quote()     
-
-    quote_without_name = quote_with_name.split('-')[0] 
-    b = 0
-
-    for i in range(0,len(quote_with_name)):
-      if quote_with_name[i] == '-':
-        b = 1
-      if b==1:
-        name = name + quote_with_name[i]
-      
-
-    await ctx.send(f'**"{quote_without_name}" {name}**') 
-    time.sleep(43200) #12 hours, you can set it to any amount of time(in seconds)
+  play_quotes.start(ctx)
 
 
 keep_alive()
 
-client.run('ODM4NTI5MjgayNTUxNTc4Njo0.YI8bQA.CJOxT9T_IQq9DFkoFoOT_ursky')  #this token is not correct :p . You can paste your bot's token here to make it run.
+client.run('ODMsd5MjgyNTUxNTcfds0.YI232sdA.sdfUtCADIAZnsfdnjlymiU')  #this token is not correct :p . You can paste your bot's token here to make it run.
